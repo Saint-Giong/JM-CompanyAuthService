84ccff9b788ca69df3b3f911bd8b77867ee4079b
NgocNguyen <s3924436@rmit.edu.vn>
2025-12-08 15:36:33 +0700

    add: init email sending service

---
+++ CompanyAuthService/src/main/java/rmit/saintgiong/authservice/common/util/EmailService.java
@@ -0,0 +1,63 @@
+package rmit.saintgiong.authservice.common.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sesv2.SesV2Client;
+import software.amazon.awssdk.services.sesv2.model.*;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class EmailService {
+
+    @Value("${aws.accessKeyId}")
+    private String accessKey;
+
+    @Value("${aws.secretAccessKey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${aws.ses.sender}")
+    private String senderEmail;
+
+    private SesV2Client sesClient;
+
+    @PostConstruct
+    public void init() {
+        this.sesClient = SesV2Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+
+    public void sendVerificationEmail(String recipientEmail, String userName, String activationLink) {
+
+        // Prepare Data
+        String templateData = String.format("{\"name\":\"%s\", \"link\":\"%s\"}", userName, activationLink);
+
+        //Build Request
+        SendEmailRequest request = SendEmailRequest.builder()
+                .fromEmailAddress(senderEmail)
+                .destination(d -> d.toAddresses(recipientEmail))
+                .content(c -> c.template(t -> t
+                        .templateName("RegistrationTemplate") // Must match AWS Console Name
+                        .templateData(templateData)
+                ))
+                .build();
+
+        //Send
+        try {
+            sesClient.sendEmail(request);
+            System.out.println("Email sent to " + recipientEmail);
+        } catch (SesV2Exception e) {
+            System.err.println("AWS SES Error: " + e.awsErrorDetails().errorMessage());
+            throw e;
+        }
+    }
+}
