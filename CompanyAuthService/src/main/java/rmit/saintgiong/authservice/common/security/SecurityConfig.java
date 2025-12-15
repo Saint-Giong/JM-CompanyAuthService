273717c9a2df37f1729a58d3c1203141b469f3b8
sontungN <s3979348@rmit.edu.vn>
2025-12-15 15:24:14 +0700

    feat: update RsaKeyLoader to load keys from environment variables and enhance SecurityConfig for OAuth2 login

--- CompanyAuthService/src/main/java/rmit/saintgiong/authservice/common/security/SecurityConfig.java
+++ CompanyAuthService/src/main/java/rmit/saintgiong/authservice/common/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package rmit.saintgiong.authservice.common.security;
 import io.jsonwebtoken.lang.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,8 +21,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic(basic -> {
-                })
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(
                         cors -> cors
@@ -38,8 +37,10 @@ public class SecurityConfig {
                                 "/v3/api-docs/**"
                         ).permitAll()
                         .requestMatchers("/register").permitAll()
+                        .requestMatchers("/hello").authenticated()
                         .anyRequest().permitAll()
-                );
+                )
+                .oauth2Login(Customizer.withDefaults());
 
         return http.build();
     }
