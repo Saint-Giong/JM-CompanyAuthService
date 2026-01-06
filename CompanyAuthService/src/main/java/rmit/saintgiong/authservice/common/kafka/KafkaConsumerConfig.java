b9a2db5cb16960c6c577fb33bc02be26f5884d1a
sontungN <s3979348@rmit.edu.vn>
2026-01-06 16:56:20 +0700

    chore: update Kafka consumer and producer configurations to use dynamic service names and add Maven backup files to ignore

--- CompanyAuthService/src/main/java/rmit/saintgiong/authservice/common/kafka/KafkaConsumerConfig.java
+++ CompanyAuthService/src/main/java/rmit/saintgiong/authservice/common/kafka/KafkaConsumerConfig.java
@@ -11,6 +11,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
@@ -29,12 +30,15 @@ public class KafkaConsumerConfig {
 
     @Value("${kafka.schema-registry-host-url}")
     private String schemaRegistryHostUrl;
+
+    @Value("${spring.application.name}")
+    private String serviceName;
     
     @Bean
     public ConsumerFactory<String, Object> authConsumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHostUrl);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "auth-service-consumer-group");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, serviceName + "-consumer-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
 
@@ -50,18 +54,21 @@ public class KafkaConsumerConfig {
         
         return new DefaultKafkaConsumerFactory<>(props);
     }
-    
+
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> authKafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, Object> authKafkaListenerContainerFactory(KafkaTemplate<String, Object> kafkaTemplate) {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(authConsumerFactory());
+        factory.setReplyTemplate(kafkaTemplate);
         return factory;
     }
 
     @Bean
     public ConcurrentMessageListenerContainer<String, Object> replyContainer(ConsumerFactory<String, Object> consumerFactory) {
-        ContainerProperties containerProperties = new ContainerProperties(KafkaTopic.COMPANY_REGISTRATION_REPLY_TOPIC);
-        containerProperties.setGroupId("auth-service-reply-group");
+        ContainerProperties containerProperties = new ContainerProperties(
+                KafkaTopic.COMPANY_REGISTRATION_REPLY_TOPIC
+        );
+        containerProperties.setGroupId(serviceName + "-reply-group");
 
         return new ConcurrentMessageListenerContainer<>(consumerFactory, containerProperties);
     }
