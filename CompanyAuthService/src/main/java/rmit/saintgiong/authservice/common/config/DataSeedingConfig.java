fa0a88f829743223f9939e6f0bdbec0cdb5f1d8a
NgocNguyen <s3924436@rmit.edu.vn>
2026-01-12 17:28:10 +0700

    feat: data seeding

--- CompanyAuthService/src/main/java/rmit/saintgiong/authservice/common/config/DataSeedingConfig.java
+++ CompanyAuthService/src/main/java/rmit/saintgiong/authservice/common/config/DataSeedingConfig.java
@@ -20,13 +20,58 @@ import java.util.UUID;
 public class DataSeedingConfig implements CommandLineRunner {
 
     private final CompanyAuthRepository companyAuthRepository;
-    private final PasswordEncoder encoder;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void run (String @NonNull ... args) {
         if (companyAuthRepository.count() != 0) {
             return;
         }
+
+        List<CompanyAuthEntity> seeds = List.of(
+                CompanyAuthEntity.builder()
+                        .companyId(UUID.fromString("22222222-2222-2222-2222-222222222222"))
+                        .email("google@gmail.com") // Company admin email
+                        .hashedPassword(passwordEncoder.encode("Google123!"))
+                        .ssoToken(null)
+                        .isActivated(true)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build(),
+
+                CompanyAuthEntity.builder()
+                        .companyId(UUID.fromString("33333333-3333-3333-3333-333333333333"))
+                        .email("netcompany@gmail.com")
+                        .hashedPassword(passwordEncoder.encode("Netcompany123!"))
+                        .ssoToken(null)
+                        .isActivated(true)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build(),
+
+                CompanyAuthEntity.builder()
+                        .companyId(UUID.fromString("44444444-4444-4444-4444-444444444444"))
+                        .email("shopee@gmail.com")
+                        .hashedPassword(passwordEncoder.encode("Shopee123!"))
+                        .ssoToken(null)
+                        .isActivated(true)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build(),
+
+                CompanyAuthEntity.builder()
+                        .companyId(UUID.fromString("55555555-5555-5555-5555-555555555555"))
+                        .email("highlandscoffee@gmail.com")
+                        .hashedPassword(passwordEncoder.encode("Highlands123!"))
+                        .ssoToken(null)
+                        .isActivated(true)
+                        .createdAt(LocalDateTime.now())
+                        .updatedAt(LocalDateTime.now())
+                        .build()
+        );
+
+        companyAuthRepository.saveAll(seeds);
+        log.info("Seeded {} distinct company accounts.", seeds.size());
 
 //        List<CompanyAuthEntity> seeds = new ArrayList<>();
 //        String uuidBased = "00000000-0000-0000-0000-00000000000";
